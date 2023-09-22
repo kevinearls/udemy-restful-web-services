6a31b35385fb2b96a230bb43bb0e24d2bdc3f9a6
Kevin Earls <kevinearls@mac.com>
2023-09-22 17:37:34 +0200

    Internationalization example

--- src/main/java/dev/kearls/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/dev/kearls/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,16 +2,33 @@ package dev.kearls.restfulwebservices.helloworld;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
     @Autowired
     private HelloWorld hello = new HelloWorld();
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-intl")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        var message = messageSource.getMessage("good.morning.message", null, "Default good morning message", locale);
+        return message;
     }
 
     @GetMapping("/hello-world-bean")
