c8e7245d99abdd1ae7fcd55a80d3d8f88fa5c666
Kevin Earls <kevinearls@mac.com>
2023-09-25 16:05:44 +0200

    Add other versioning approaches
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- src/main/java/dev/kearls/restfulwebservices/VersioningPersonController.java
+++ src/main/java/dev/kearls/restfulwebservices/VersioningPersonController.java
@@ -15,4 +15,28 @@ public class VersioningPersonController {
         Name name = new Name("Barney", "RUBBLE");
         return new PersonV2(name);
     }
+
+    // Versioning by request parameter http://localhost:8080/person?version=1
+    @GetMapping(value = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonByParameter() {
+        return new PersonV1("Fred Flinstone I");
+    }
+
+    // Versioning by request header X-API-VERSION=2
+    // curl -H"X-API-VERSION: 2" http://localhost:8080/person | jq
+    @GetMapping(value = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonByHeader() {
+        Name name = new Name("Betty", "RUBBLE");
+        return new PersonV2(name);
+    }
+
+    // Versioning by setting the accept header
+    //
+    @GetMapping(value = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        Name name = new Name("Mr.", "SLATE");
+        return new PersonV2(name);
+    }
+
+
 }
