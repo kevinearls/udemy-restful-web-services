19dc86c9c2d784445bf71ff686799ba5582df093
Kevin Earls <kevinearls@mac.com>
2023-09-25 17:58:08 +0200

    Dynamic filtering
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- src/main/java/dev/kearls/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/dev/kearls/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package dev.kearls.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +12,14 @@ import java.util.List;
 @RestController
 public class FilteringController {
     @GetMapping("/filtering")
-    public SomeBean filtering() {
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue filtering() {
+        var someBean = new SomeBean("value1", "value2", "value3");
+        var mappingJacksonValue = new MappingJacksonValue(someBean);
+        var filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 
     @GetMapping("filtering-list")
--- src/main/java/dev/kearls/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/dev/kearls/restfulwebservices/filtering/SomeBean.java
@@ -1,10 +1,12 @@
 package dev.kearls.restfulwebservices.filtering;
 
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(value = { "field2" })
+//@JsonIgnoreProperties(value = { "field2" })
+@JsonFilter("SomeBeanFilter")
 public record SomeBean(String field1, String field2, String field3) {
 
 }
